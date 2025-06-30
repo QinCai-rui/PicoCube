809cdd641fec0a4d747de2065d3cf938bba79a0c
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-06-30 12:45:34 +1200

    bug fix

--- pi02w/raspicube/oled_shutdown.py
+++ pi02w/raspicube/oled_shutdown.py
@@ -64,7 +64,7 @@ def main():
                 show_shutdown_notice()
                 time.sleep(0.7)
                 logging.info("Calling system shutdown.")
-                subprocess.Popen(["systemctl", "poweroff"])
+                subprocess.Popen(["sudo", "systemctl", "poweroff"])
                 break
             time.sleep(0.05)
     except Exception as e:
